93cbe95e1adce3a4442827371a36f65243b8a6c8
Massimiliano Giorgi <m.giorgi@mgsi.eu>
2019-09-25 12:17:03 +0200

    Added privapp permission for me.phh.treble.app for lineage.

--- lineage.mk
+++ lineage.mk
@@ -1,3 +1,5 @@
+PRODUCT_COPY_FILES += \
+	device/phh/treble/config/permissions/privapp-permissions-me.phh.treble.app.xml:system/etc/permissions/privapp-permissions-me.phh.treble.app.xml
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 $(call inherit-product, device/lineage/sepolicy/common/sepolicy.mk)
 -include vendor/lineage/build/core/config.mk
